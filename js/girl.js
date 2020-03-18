caaa53cff319ffbf41856a6f094a402a67cfb5a2
MIchaelFU0403 <1078537664@qq.com>
2020-03-18 22:23:09 +0800

    Site updated: 2020-03-18 22:23:09

---
+++ js/girl.js
@@ -0,0 +1,3 @@
+
+<script type="text/javascript" charset="utf-8" async="" src="https://cdn.jsdelivr.net/npm/live2d-widget@3.0.4/lib/L2Dwidget.0.min.js"></script>
+<script type="text/javascript"  src="https://cdn.jsdelivr.net/npm/live2d-widget@3.0.4/lib/L2Dwidget.min.js?_=1557308476616"></script>
